f004ed218ed1c126532daccb132c0d0b7e16c9df
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2018-03-30 20:41:15 -0300

    Adapter

--- financasK/app/src/main/java/br/com/alura/financask/ui/adapter/ListaTransacoesAdapter.kt
+++ financasK/app/src/main/java/br/com/alura/financask/ui/adapter/ListaTransacoesAdapter.kt
@@ -1,5 +1,25 @@
 package br.com.alura.financask.ui.adapter
 
-class ListaTransacoesAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class ListaTransacoesAdapter : BaseAdapter() {
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItem(p0: Int): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemId(p0: Int): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 
 }
